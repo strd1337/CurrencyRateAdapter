bb7a1d98d3916d0e43907b718f1e7b38439f8721
StrdL <97736243+strd1337@users.noreply.github.com>
2024-10-20 03:06:47 +0300

    Altered docker (#4)

--- src/CurrencyRateAdapter.Presentation/Program.cs
+++ src/CurrencyRateAdapter.Presentation/Program.cs
@@ -18,11 +18,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyRateAdapter API V1"));
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyRateAdapter API V1"));
+//}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyRateAdapter API V1"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
